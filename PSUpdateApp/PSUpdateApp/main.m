070bca178c0872332a7612158b3004e555e463ba
danielebogo <bogodaniele83@gmail.com>
2013-02-18 18:13:52 +0100

    First commit

---
+++ PSUpdateApp/PSUpdateApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PSUpdateApp
+//
+//  Created by iBo on 18/02/13.
+//  Copyright (c) 2013 D-Still. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
